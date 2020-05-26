cf79bffad025421968da5cf2fc611edfb05069b4
blaker814 <54994930+blaker814@users.noreply.github.com>
2020-05-26 10:07:21 -0500

    first commit

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '871c0ac76ace42be822427d5c6b7dbc6'
 });
 
 const handleApiCall = (req, res) => {
